e524f1c485572e109d5ed026389e9c6315ddba1b
Rajat Shrivastav <rajatshrivastav256@gmail.com>
2026-03-23 20:36:13 +0530

    Update DAY1.py

--- DAY1.py
+++ DAY1.py
@@ -3,35 +3,35 @@ print("QUESTION")
 
 #QUE:- WRITE A PROGRAM TO TAKE 2 INPUT AND PRINT THIER SUM?
  
-# num1=int(input("ENETR NUM1:- "))
-# num2=int(input("ENTER NUM2:-"))
-# print(num1+num2)
+num1=int(input("ENETR NUM1:- "))
+num2=int(input("ENTER NUM2:-"))
+print(num1+num2)
 
 #QUE :- WAP TO INPUT SIDE OF SQUARE AND CALCULATE AREA 
-# side=float(input("ENTER SIDE OF SQUARE:-"))
-# print("AREA OF SQuARE:-",side**2) #area of square = side*side
+side=float(input("ENTER SIDE OF SQUARE:-"))
+print("AREA OF SQuARE:-",side**2) #area of square = side*side
 
 #QUE:- WAP TO TAKE 2 FLOAT NUMBER AND GIVE AVERAGE
 
-# num1=float(input("ENTER NUM1:-"))
-# num2=float(input("ENTER NUM2:-"))
-# average = (num1+num2)/2
-# print("AVERAGE IS:-",average)
+num1=float(input("ENTER NUM1:-"))
+num2=float(input("ENTER NUM2:-"))
+average = (num1+num2)/2
+print("AVERAGE IS:-",average)
 
 #QUE:- WAP TO TAKE 2 INTERGER NUMBER AND PRINT TRUE IF A IS GREATER OR EQUAL TO B
 
-# a=int(input("ENTER NUM1:-"))
-# b=int(input("ENTER NUM2:-"))
-# print(a>=b)
+a=int(input("ENTER NUM1:-"))
+b=int(input("ENTER NUM2:-"))
+print(a>=b)
 
 #QUE:- CONVERT CELSIUS TO FAHERENHEIT AND VICE VERSA
 
-# TEM1=float(input("ENTER TEMP. IN CELCIUS:-"))
-# TEM2=float(input("ENTER TEMP. IN FAHERENHEIT:-"))
-# F=(TEM1+1.8)*32
-# C=(TEM2-32)/1.8
-# print("TEM1 IN FAHERE.:-",F)
-# print("TEM2 IN CELCIUS:-",C)
+TEM1=float(input("ENTER TEMP. IN CELCIUS:-"))
+TEM2=float(input("ENTER TEMP. IN FAHERENHEIT:-"))
+F=(TEM1+1.8)*32
+C=(TEM2-32)/1.8
+print("TEM1 IN FAHERE.:-",F)
+print("TEM2 IN CELCIUS:-",C)
 
 #QUE:- PRINT DOUBLE,TRIPLE,AND SQUARE
 
