40c631813dcc5ecbb726130aee242faf2aff5eac
Rajat Shrivastav <rajatshrivastav256@gmail.com>
2026-03-23 20:33:34 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -16,14 +16,14 @@ print("Number of classes needed for unique subject:",len(unique_subject))
 
 #Q3:WAP to enter marks of 3 subjects from the user and store them in a dictionary. 
 # Start with an empty dictionary & add one by one. Use subject name as key & marks as value.
-# marks={}
-# x=float(input("ENTER NUMBER OF MATHS:"))
-# marks.update({"Maths":x})
-# z=float(input("ENTER NUMBER OF Physics:"))
-# marks.update({"Physics":z})
-# y=float(input("ENTER NUMBER OF Chemistry:"))
-# marks.update({"Chemistry":y})
-# print(marks)
+marks={}
+x=float(input("ENTER NUMBER OF MATHS:"))
+marks.update({"Maths":x})
+z=float(input("ENTER NUMBER OF Physics:"))
+marks.update({"Physics":z})
+y=float(input("ENTER NUMBER OF Chemistry:"))
+marks.update({"Chemistry":y})
+print(marks)
 #Q4:Figure out a way to store 9 & 9.0 as separate values in the set.
 set={(9,"int"),(9.0,"float")}
 print(set)
